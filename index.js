c62813ff9724d9c5dad60fd8c8c7c671b2d70c5b
JuanCampoy <jmcampoy0git@gmail.com>
2022-08-24 18:20:45 -0600

    1 DESAFIO

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+let palabraRep = prompt('Ingresa tu palabra')
+let repeticionNum = parseInt(prompt('Ingresa el numero de veces que quieres repetir la palabra'))
+
+for(let iteracion = 0;iteracion<=repeticionNum;iteracion++){
+        alert('tu palabra  ' + palabraRep + '  numero  ' +iteracion)
+
+         console.log('iteracion numero: '+iteracion)
+    }
+
+    
